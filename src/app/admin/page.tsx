1ddb01ec596d2ecad682883ae185c49b44096d59
liamtsang <lt1800@nyu.edu>
2025-01-10 13:58:16 -0500

    admin table

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -14,21 +14,39 @@ export default async function Page() {
 
 function AdminGrid({ results }: { results: unknown[] }) {
 	return (
-		<div>
+		<main className="grid grid-cols-1">
+			<div className="grid grid-cols-[1fr_1fr_2fr_2fr]">
+				<div className="p-1 bg-[var(--bm-white)] text-[var(--bm-black)] border border-[var(--bm-white)]">
+					Email
+				</div>
+				<div className="p-1 bg-[var(--bm-white)] text-[var(--bm-black)] border border-[var(--bm-white)]">
+					Answers
+				</div>
+				<div className="p-1 bg-[var(--bm-white)] text-[var(--bm-black)] border border-[var(--bm-white)]">
+					ID
+				</div>
+				<div className="p-1 bg-[var(--bm-white)] text-[var(--bm-black)] border border-[var(--bm-white)]">
+					Timestamp
+				</div>
+			</div>
 			{results.map((row) => (
 				<GridRow key="row" row={row as unknown as surveyResults} />
 			))}
-		</div>
+		</main>
 	);
 }
 
 function GridRow({ row }: { row: surveyResults }) {
 	return (
-		<div>
-			<div>{row.id}</div>
-			<div>{row.email}</div>
-			<div>{row.survey_answers}</div>
-			<div>{row.timestamp}</div>
+		<div className="grid grid-cols-[1fr_1fr_2fr_2fr]">
+			<div className="p-1 border border-[var(--bm-white)]">{row.email}</div>
+			<div className="p-1 border border-[var(--bm-white)]">
+				<a className="text-sky-400" href={`/admin/${row.id}`}>
+					View Answers
+				</a>
+			</div>
+			<div className="p-1 border border-[var(--bm-white)]">{row.id}</div>
+			<div className="p-1 border border-[var(--bm-white)]">{row.timestamp}</div>
 		</div>
 	);
 }
